24d4ed4c844a7d4f1563c669f00be52c6a022679
Andres <andreslin444@gmail.com>
2022-05-16 13:04:36 +0200

    Login con diversos privilegios

--- node-firstwebsite/src/index.js
+++ node-firstwebsite/src/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 const app = express(); // servidor
 const path = require('path'); // modulo para path
 const morgan = require('morgan');
+const expresValidator = require("express-validator");
+app.use(cookieParser());
+app.use( session({ // session para la app con sus atributos
+    secret: "SECRET",
+    saveUninitialized: false,
+    resave: true,
+    admin: false
+}))
 app.set('port', 3000); // puerto en el que escucha
 
 app.set('views', path.join(__dirname, 'pages')); // redefinir donde estas las paginas
--- node-firstwebsite/src/routes/index.js
+++ node-firstwebsite/src/routes/index.js
@@ -1,25 +1,53 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res)=>{
-    //res.sendFile(path.join(__dirname,'pages/index.html'));
+// variable para autenticar usuario y password
+var auth = function(req, res, next) {
+    if (req.session && (req.session.user === "amy" || req.session.user ==="pepe"))
+      return next();
+    else
+      return res.sendStatus(401);
+};
+// pagina para logear
+router.get('/log',(req, res)=>{
+    req.session.destroy();
+    res.render('log',{title : "LOGIN"})
+    
+})
+// pagina de index
+router.get('/', (req, res)=>{
     res.render('index', { title : "First Website"});
 });
-router.get('/contact', (req,res)=>{
-    //res.sendFile(path.join(__dirname,'pages/index.html'));
-    
-    res.render('contact', { title : "Contact pages"});
-});
-router.get('/login', (req, res)=>{
-    res.render('login', { title: "Login"});
-});
-router.post('/login', (req, res)=>{
-    var data = req.body;
-    if(data.password === 'password'){
-        res.render('login',{ title: "Login correcto"});
+// pagina de contact
+router.get('/contact', auth, (req, res)=>{
+    if(req.session.admin){
+        res.render('contact', { title : "Contact pages for admins"});
     }else{
-        res.render('login',{ title: "Login inccorrecto"});
+        res.render('contact', { title : "Contact pages for users"});
+    }
+});
+
+
+router.post('/loggedin', function(req, res){
+    var data = req.body;
+
+    if(!data.username || !data.password){
+        res.send("Introduzca datos <a href='log'>VOLVER</a>")
+    }else{
+        if(data.username === "amy" && data.password ==="password"){
+            req.session.user = "amy";
+            req.session.admin = true;
+            res.render('loggedin',{ desc : "LOGOUT from ADMIN", title: "LOGGED."})
+
+        }else if(data.username ==="pepe" && data.password === "password"){
+            req.session.user ="pepe";
+            res.render('loggedin',{ desc : "LOGOUT from normal user", title: "LOGGED."})
+        }else{
+            res.send("Valores incorrectos <a href='log'>Volver</a>")
+           
+        }
     }
     
 })
-module.exports = router;
+
+module.exports = router; // exporta los datos de router
