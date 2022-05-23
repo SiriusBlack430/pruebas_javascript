11cda1f2116f0f483102c01ce008cd0141113d8e
Andres <andreslin444@gmail.com>
2022-05-23 13:02:13 +0200

    Cambiar contraseña

--- node-firstwebsite/src/routes/index.js
+++ node-firstwebsite/src/routes/index.js
@@ -57,7 +57,7 @@ router.get('/contact', auth, (req, res)=>{
 });
 
 router.get('/userList', authAdmin, async (req, res)=>{
-    var User = await pool.query("SELECT * FROM USER");
+    var User = await pool.query("SELECT id,username,permiss FROM USER");
 
     res.render("userList",{ user : User});
 
@@ -126,6 +126,64 @@ router.post('/loggedin', async function(req, res){
         }
     }
     
+})
+
+router.get('/userList/delete', async (req,res)=>{
+    const id = req.query.id;
+    var User = await pool.query("SELECT permiss FROM USER WHERE id="+id);
+
+    if(User){
+        if(User[0].permiss != "ADMIN"){
+           await pool.query("DELETE FROM USER WHERE id=" + id);
+        }
+      
+    }else{
+       console.log("NO ENCONTRADO")
+    }
+    
+    res.redirect("/userList");
+
+})
+router.get('/userList/edit', async (req,res)=>{
+    const id = req.query.id;
+    var User = await pool.query("SELECT id,username,permiss FROM USER WHERE id="+id);
+ 
+    res.render('edit',{user: User[0]});
+   
+
+})
+//Modificar username y permisos
+router.post('/userList',authAdmin, async(req,res)=>{
+    const id = req.query.id;
+    var data = req.body;
+    await pool.query("UPDATE USER Set username ='" + data.username +"' ,permiss ='" + data.privileges + "' WHERE id=" + id);
+    res.redirect("/userList");
+})
+
+router.get('/userList/edit/passChange' , async(req,res)=>{
+    const id = req.query.id;
+    var User = await pool.query("SELECT id,username,permiss FROM USER WHERE id=" + id);
+
+    res.render('passChange',{user : User[0],passNotMatch:false,actual:false});
+})
+//Cambiar contraseña
+router.post('/userList/edit', async (req,res)=>{
+    const id = req.query.id;
+    const data = req.body;
+    const User = await pool.query("SELECT * FROM USER WHERE id="+id);
+    const compare = await bcrypt.compare(data.actualPass,User[0].password);
+    if(compare){
+        if(data.newPass == data.confirmNewPass){
+            const hashedPassword = await bcrypt.hash(data.newPassword,saltRounds);
+            await pool.query("UPDATE USER Set password ='" + hashedPassword +"' WHERE id=" + id);
+            res.redirect('/userList/edit');
+        }else{
+            res.render('passChange',{user : User[0],passNotMatch:true,actual:false});
+            return;
+        }
+    }
+    res.render('passChange',{user: User[0],passNotMatch:false,actual : true})
+
 })
 
 
