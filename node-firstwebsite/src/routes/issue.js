8f346a37c862f4b2ec1f3b7d0508245e45b45b65
Andres <cf18andres.lin@iesjoandaustria.org>
2022-05-27 00:04:36 +0200

    Obtener las issues

--- node-firstwebsite/src/routes/issue.js
+++ node-firstwebsite/src/routes/issue.js
@@ -1,6 +1,28 @@
 const express = require('express');
+const https = require('https');
 const router = express.Router();
-router.get('/issue',(req,res)=>{
-    res.render('issue');
+const {Octokit} = require('octokit');
+
+
+const octokit = new Octokit({
+    auth: "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"
 })
+// token
+// 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
+router.get('/issue', async(req,res)=>{
+    const respuesta = await octokit.request('GET /repos/{owner}/{repo}/projects/{project_id}',{
+        owner: 'SiriusBlack430',
+        repo: 'pruebas_javascript',
+        project_id: '1'
+    })
+    console.log(respuesta)
+})
+async function run(){
+    const data = await octokit.request("GET /repos/{owner}/{repo}/issues",{
+        owner: 'SiriusBlack430',
+        repo: 'pruebas_javascript',   
+    });
+    console.log(data);
+}
+run()
 module.exports = router;
