77317a18943d4c527bd7183b3bb893a196fb860e
Andres <andreslin444@gmail.com>
2022-05-27 20:46:32 +0200

    GITHUB PROJECT

--- node-firstwebsite/src/routes/issue.js
+++ node-firstwebsite/src/routes/issue.js
@@ -1,11 +1,14 @@
+const { query } = require('express');
 const express = require('express');
 const https = require('https');
 const router = express.Router();
 const {Octokit} = require('octokit');
-
+const graph = require('graphql');
 
 const octokit = new Octokit({
-    auth: "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"
+    // mi ordenador auth: "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"
+    //ordenador empresa
+    auth: "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"
 })
 // token
 // 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
@@ -18,11 +21,23 @@ router.get('/issue', async(req,res)=>{
     console.log(respuesta)
 })
 async function run(){
-    const data = await octokit.request("GET /repos/{owner}/{repo}/issues",{
-        owner: 'SiriusBlack430',
-        repo: 'pruebas_javascript',   
-    });
-    console.log(data);
+    try{
+        const response = await octokit.request("GET /repos/{owner}/{repo}/projects",{
+            
+            owner: 'SiriusBlack430',
+            repo: 'pruebas_javascript'   
+            //project_id:1
+        });
+        
+        console.log(response);
+    }catch(e){
+        console.log(e)
+    } 
+
 }
 run()
 module.exports = router;
+curl --request POST --url https://api.github.com/repos/SiriusBlack430/pruebas_javascript --header "Authorization: token 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 " --data '{"query":"query{user(login: \"SiriusBlack430\") {projectNext(number: NUMBER){id}}}"}'
+
+
+curl --request POST --url https://api.github.com/graphql --header 'Authorization: token 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' --data '{"query":"query{user(login: \"SiriusBlack430\") {projectNext(number: NUMBER){id}}}"}'
