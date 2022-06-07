95f79d3b8e9883fa9ee8da5f00c1f80ecb5268b4
Andres <andreslin444@gmail.com>
2022-06-07 11:25:09 +0200

    Tabla con config

--- node-firstwebsite/src/routes/issue.js
+++ node-firstwebsite/src/routes/issue.js
@@ -1,52 +1,64 @@
 const express = require('express');
-const { render } = require('express/lib/response');
 const router = express.Router();
 const fetch = require('node-fetch');
 const pool = require("./conection");
 
-
-
-
-var github_data ={};
+var github_data ={}, headers = {};;
 const baseUrl = "https://api.github.com/graphql"; // url api
-var headers = {};
+
 async function initializeData(){
   //datos de config
   const data = await pool.query("SELECT * FROM REPCONFIG");
+  var auth = "bearer " + data[0].token;
   headers = {
     "Content-Type":"application/json",
-    Authorization: "bearer " + data[0].token,
+    Authorization: auth
   }
   //obtener id project a partir del nombre del proyecto
-  const info = await fetch(baseUrl,{
-    method: "POST",
-    headers: headers,
-    body: JSON.stringify(idProject(data[0].name))
-  })
-  var infoJson = await info.json();
-  infoJson = infoJson.data.user.projectNext.id
-  github_data = {
-    "token": data[0].token,
-    "username": data[0].name,
-    "projectId": infoJson
+  try{
+    const info = await fetch(baseUrl,{
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(idProject(data[0].name))
+    })
+    var infoJson = await info.json();
+    infoJson = infoJson.data.user.projectsNext.nodes;
+    var projectId;
+    for(let i in infoJson){
+      if(infoJson[i].title == data[0].projectName){
+        projectId = infoJson[i].id;
+      }
+    }
+    github_data = {
+      "token": data[0].token,
+      "username": data[0].name,
+      "projectId": projectId
+    }
+  }catch(e){
+    github_data = {}
+    console.log("Configuracion incorrecta")
   }
+  
 }
 initializeData()
 // query graphql
+//id project  PN_kwHOBQI_A84ACKFd
 function idProject(username){
  return {
   "query":`
   query{
     user(login:"`+ username+`") {
-      projectNext(number: 1) {
-        id
-        title
+      projectsNext(first:20) {
+        nodes{
+          id
+          title
+        }    
       }
     }
   }`,
 };
 }
-function statusIssueOptions(){
+function nameStatusLabelUrl(){
   return {"query":`
     query{
       node(id: "${github_data.projectId}") {
@@ -58,43 +70,38 @@ function statusIssueOptions(){
               settings
             }
           }
-        }
-      }
-    }`
-  }
-};
-
-function statusId_value(){
-  return {
-    "query" : `query{
-      node(id: "${github_data.projectId}") {
-        ... on ProjectNext {
           items(first: 20) {
-            edges {
-              node {
-                title
-                fieldValues(first: 20) {
-                  nodes {
-                    projectField {
+            nodes {
+              fieldValues(first: 8) {
+                nodes {
+                  value
+                  projectField {
+                    name
+                  }
+                }
+              }
+              content {
+                ... on Issue {
+                  title
+                  labels(first: 5) {
+                    nodes {
                       name
                     }
-                    value
                   }
+                  bodyUrl
                 }
               }
             }
           }
         }
       }
-    }`,
-  };
-}
+    }`
+  }
+};
+
 router.get("/configRepos",async(req,res)=>{
   var config = await pool.query("SELECT * from REPCONFIG");
   res.render("configRepos",{config:config[0]})
-})
-router.get("/issue",(req,res)=>{
-  res.render("issue")
 })
 router.post("/issue",async (req,res)=>{
   var data = req.body;
@@ -103,82 +110,58 @@ router.post("/issue",async (req,res)=>{
   }catch(e){
     console.log(e);
   }
-  res.render("issue")
+
+  await initializeData()
+  res.redirect("issue")
+  
 })
 
 
-
-router.get("/issueStatusOptions", async(req,res)=>{
-  const info = await fetch(baseUrl,{
-    method: "POST",
-    headers: headers,
-    body: JSON.stringify(statusIssueOptions())
-  })
-  const infoJson = await info.json();
-  var object = infoJson.data.node.fields.nodes;
-  var statusOptions;
-  for(var i=0;i<object.length;i++){
-    if(object[i].name ==="Status"){
-      statusOptions = JSON.parse(object[i].settings).options;
-    }
-  }
-  for(var i=0;i<statusOptions.length;i++){
-    delete statusOptions[i].name_html;
-  }
-
-  res.send(statusOptions);
-});
-
-router.get("/issueStatus" ,async(req,res)=>{
-  const infoStat = await fetch(baseUrl,{
-    method: "POST",
-    headers: headers,
-    body: JSON.stringify(statusIssueOptions())
-  })
-  const infoStatJson = await infoStat.json();
-  var object = infoStatJson.data.node.fields.nodes;
-  var statusOptions;
-  for(var i=0;i<object.length;i++){
-    if(object[i].name ==="Status"){
-      statusOptions = JSON.parse(object[i].settings).options;
-    }
-  }
-  for(var i=0;i<statusOptions.length;i++){
-    delete statusOptions[i].name_html;
-  }
-
-
-  const info = await fetch(baseUrl,{
-    method: "POST",
-    headers: headers,
-    body: JSON.stringify(statusId_value())
-  })
-  const infoJson = await info.json();
-  var object = infoJson.data.node.items.edges;
+router.get("/issue" ,async(req,res)=>{
   var element =[],cart={}
-  for(var i=0;i<object.length;i++){
-    for(var j=0;j<object[i].node.fieldValues.nodes.length;j++){
-      if(object[i].node.fieldValues.nodes[j].projectField.name =="Status"){
-        
-        cart.title = object[i].node.title;
-        for(var m=0;m<statusOptions.length;m++){
-          if(statusOptions[m].id===object[i].node.fieldValues.nodes[j].value){
-            cart.status = statusOptions[m].name;
-          }
-        }
-        element.push({title: cart.title,status: cart.status});
-        
-        
+  try{
+
+    const info = await fetch(baseUrl,{
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(nameStatusLabelUrl())
+    })
+    const infoJson = await info.json();
+    var object = infoJson.data.node.fields.nodes;
+    var statusOptions;
+    for(var i=0;i<object.length;i++){
+      if(object[i].name ==="Status"){
+        statusOptions = JSON.parse(object[i].settings).options;
       }
     }
-    
-    
+    for(var i=0;i<statusOptions.length;i++){
+      delete statusOptions[i].name_html;
+    }
+    object = infoJson.data.node.items.nodes;
+    for(var i=0;i<object.length;i++){
+      for(var j=0;j<object[i].fieldValues.nodes.length;j++){
+        if(object[i].fieldValues.nodes[j].projectField.name =="Status"){
+          cart.label = "";
+          cart.title = object[i].content.title;
+          cart.url = object[i].content.bodyUrl;
+          for(var o =0;o<object[i].content.labels.nodes.length;o++){
+            cart.label = cart.label +" "+ object[i].content.labels.nodes[o].name
+          }
+
+          for(var m=0;m<statusOptions.length;m++){
+            if(statusOptions[m].id===object[i].fieldValues.nodes[j].value){
+              cart.status = statusOptions[m].name;
+            }
+          }
+          element.push({title: cart.title,status: cart.status,url: cart.url,label: cart.label});
+        }
+      }
+    }
+    res.render("issue",{element})
+  }catch(e){
+    res.render("issue",{element: {}})
   }
-  res.send(element)
-
-
+  
 });
-//   auth: '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' 
-
-
+//   auth: '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' 
 module.exports = router;
