3cd0ef2db042bd767e18b5736ea419765c0636d6
Andres <andreslin444@gmail.com>
2022-06-09 10:18:03 +0200

    auth y repconfig sin config

---
+++ ConversionJson/fake_data_AddNextGen.js
@@ -0,0 +1,50 @@
+const data_client_W0001 = [
+	{
+		flavor: 'MiniGT',
+		username: '00007611',
+		ts: '2022-02-10 00:00:00.000'
+	},
+	{
+		flavor: 'MiniGT',
+		username: '00008723',
+		ts: '2022-02-10 00:00:00.000'
+	},
+	{
+		flavor: 'MiniGT',
+		username: 'empleado1',
+		ts: '2022-02-11 00:00:00.000'
+	},
+	{
+		flavor: 'BoldGT',
+		username: 'empleado3',
+		ts: '2022-02-11 00:00:00.000'
+	}
+]
+
+const data_client_W0002 = [
+	{
+		flavor: 'BoldGT',
+		username: '00007611',
+		ts: '2022-02-10 00:00:00.000'
+	},
+	{
+		flavor: 'BoldGT',
+		username: '00008723',
+		ts: '2022-02-10 00:00:00.000'
+	},
+	{
+		flavor: 'BoldGT',
+		username: 'empleado1',
+		ts: '2022-02-11 00:00:00.000'
+	},
+	{
+		flavor: 'BoldGT',
+		username: 'empleado3',
+		ts: '2022-02-11 00:00:00.000'
+	}
+]
+
+module.exports = {
+	data_client_W0001,
+	data_client_W0002
+}
